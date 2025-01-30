cb363c42507bb69ba852a4ea60afa7caf873f1de
Vitória Gonçalves Passos <VitoriaGoncalvesPassos@hotmail.com>
2025-01-30 05:30:24 -0300

    initial commit

---
+++ WebApiCadastro/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiCadastro.DataContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add dependências.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//dizendo para utilizarmos as nossas configurações do appsettings
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
